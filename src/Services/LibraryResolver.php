06adbe2888154d14078cde0f566ac36e5173246b
stephane888 <kksasteph888@gmail.com>
2021-09-29 16:12:13 +0100

    MAJ

--- src/Services/LibraryResolver.php
+++ src/Services/LibraryResolver.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Drupal\grapesjs_editor\Services;
 
 use Drupal\Core\Asset\AssetResolverInterface;
@@ -11,70 +10,73 @@ use Drupal\Core\Url;
 /**
  * Defines a class to retrieve default theme libraries.
  */
-class LibraryResolver {
+class LibraryResolver
+{
 
-  /**
-   * The theme handler service.
-   *
-   * @var \Drupal\Core\Extension\ThemeHandlerInterface
-   */
-  protected $themeHandler;
+    /**
+     * The theme handler service.
+     *
+     * @var \Drupal\Core\Extension\ThemeHandlerInterface
+     */
+    protected $themeHandler;
 
-  /**
-   * The theme initialization service.
-   *
-   * @var \Drupal\Core\Theme\ThemeInitializationInterface
-   */
-  protected $themeInitialization;
+    /**
+     * The theme initialization service.
+     *
+     * @var \Drupal\Core\Theme\ThemeInitializationInterface
+     */
+    protected $themeInitialization;
 
-  /**
-   * The asset resolver service.
-   *
-   * @var \Drupal\Core\Asset\AssetResolverInterface
-   */
-  protected $assetResolver;
+    /**
+     * The asset resolver service.
+     *
+     * @var \Drupal\Core\Asset\AssetResolverInterface
+     */
+    protected $assetResolver;
 
-  /**
-   * LibraryResolver constructor.
-   *
-   * @param \Drupal\Core\Extension\ThemeHandlerInterface $theme_handler
-   *   The theme handler service.
-   * @param \Drupal\Core\Theme\ThemeInitializationInterface $theme_initialization
-   *   The theme initialization service.
-   * @param \Drupal\Core\Asset\AssetResolverInterface $asset_resolver
-   *   The asset resolver service.
-   */
-  public function __construct(ThemeHandlerInterface $theme_handler, ThemeInitializationInterface $theme_initialization, AssetResolverInterface $asset_resolver) {
-    $this->themeHandler = $theme_handler;
-    $this->themeInitialization = $theme_initialization;
-    $this->assetResolver = $asset_resolver;
-  }
-
-  /**
-   * Returns css file array.
-   *
-   * @return array
-   *   The css file array.
-   */
-  public function getStyles() {
-    /* @var \Drupal\Core\Theme\ActiveTheme $theme */
-    $default_theme = $this->themeHandler->getDefault();
-    $theme = $this->themeInitialization->initTheme($default_theme);
-    $assets = AttachedAssets::createFromRenderArray([
-      '#attached' => [
-        'library' => $theme->getLibraries(),
-      ],
-    ]);
-
-    $css = [];
-    $front = Url::fromRoute('<front>', [], ['absolute' => TRUE])->toString();
-    foreach ($this->assetResolver->getCssAssets($assets, FALSE) as $uri => $css_asset) {
-      if ($css_asset['media'] === 'all') {
-        $css[] = $front . $uri;
-      }
+    /**
+     * LibraryResolver constructor.
+     *
+     * @param \Drupal\Core\Extension\ThemeHandlerInterface $theme_handler
+     *            The theme handler service.
+     * @param \Drupal\Core\Theme\ThemeInitializationInterface $theme_initialization
+     *            The theme initialization service.
+     * @param \Drupal\Core\Asset\AssetResolverInterface $asset_resolver
+     *            The asset resolver service.
+     */
+    public function __construct(ThemeHandlerInterface $theme_handler, ThemeInitializationInterface $theme_initialization, AssetResolverInterface $asset_resolver)
+    {
+        $this->themeHandler = $theme_handler;
+        $this->themeInitialization = $theme_initialization;
+        $this->assetResolver = $asset_resolver;
     }
 
-    return $css;
-  }
+    /**
+     * Returns css file array.
+     *
+     * @return array The css file array.
+     */
+    public function getStyles()
+    {
+        /* @var \Drupal\Core\Theme\ActiveTheme $theme */
+        $default_theme = $this->themeHandler->getDefault();
+        $theme = $this->themeInitialization->initTheme($default_theme);
+        $assets = AttachedAssets::createFromRenderArray([
+            '#attached' => [
+                'library' => $theme->getLibraries()
+            ]
+        ]);
 
+        $css = [];
+        $front = Url::fromRoute('<front>', [], [
+            'absolute' => TRUE
+        ])->toString();
+        foreach ($this->assetResolver->getCssAssets($assets, FALSE) as $uri => $css_asset) {
+            if ($css_asset['media'] === 'all') {
+                // $css[] = $front . $uri;
+            }
+        }
+
+        return $css;
+    }
 }
